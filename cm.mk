967cc43fed1e1e3d3ef9568c40426cc363adf2e9
noname55804 <marlin2201@gmail.com>
2015-01-09 17:26:05 +0100

    m8: change to vendor/ch
    Change-Id: I63def861629d6216f8a331a5d2b9ea3d8a87f8bd
    Signed-off-by: noname55804 <marlin2201@gmail.com>

--- cm.mk
+++ cm.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/ch/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/ch/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_m8
