393f00f67a7be7a2521528a590dfeec6374714d2
noname55804 <marlin2201@gmail.com>
2015-03-29 19:42:49 +0200

    SaberMod test
    Change-Id: I1602ceb5914e37140a36d4f9ad5b4fb31a2ca43b
    Signed-off-by: noname55804 <marlin2201@gmail.com>

--- ch.mk
+++ ch.mk
@@ -1,3 +1,10 @@
+# Sabermod configs
+O3_OPTIMIZATIONS := true
+TARGET_SM_AND := 4.8
+TARGET_SM_KERNEL := 4.9
+TARGET_LIB_VERSION := 4.8
+ENABLE_PTHREAD := true
+
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
 # Enhanced NFC
