308469aac2392a730447d1ace8fedd8c0b619b7c
noname55804 <marlin2201@gmail.com>
2015-04-03 12:22:16 +0200

    CYANHACKER HACKIFY OPTIMIZATIONS V2 (3/4)
    In General:
    Revert "Hackify Optimizations V1"
    Add SaberMod Optimizations V4
    Add some parts of Archidroid optimizations V4
    
    In detail:
    
    build repo:
    -update HOST toolchains from the far outdated 4.6 to 4.8
    -also Update GLIBC toolchains from 2.11-4.6 to 2.20-4.8
    -add O3 optimizations
    -add pthread and graphite to improve performance noticable
    -build with thumb-interwork
    -add Custom toolchain support for ROM and Kernel
    -show the used toolchains at the beginning of each build
    -and more...!
    
    vendor repo:
    -introduce sm.mk which handles all the flags and the common LOCAL_DISABLE flags
    -if SM toolchains are used, print version number to build.prop
    -print optimization levels to build.prop
    -update the buildscript
    -HACKIFY isnt needed anymore
    -we only want to build userdebug variants
    -add an option to remove ~/.ccache
    
    device repo:
    -allow to set TARGET_GCC_ARM and TARGET_GCC_AND (Kernel and ROM toolchain)
    -allow to set TARGET_LIB_VERSION (although it always should be set to the same version number as the ROM toolchain
    -enable pthread or not
    -for old devices -> USE_LEGACY_GCC
    
    kernel repo:
    -add Graphite flags
    -add O3 flags
    -and some more minor patches
    
    CREDITS:
    All Credit goes to the SaberMod team and to the ArchiDroid team
    https://gitlab.com/groups/SaberMod
    https://github.com/ArchiDroid/android_build/commit/a062cc61184950df02c39a038c5bfaef5a8b268c
    
    special thanks to:
    -Paul Beeler (Saber-Dev)
    -Joe Maples (frap129)
    
    Change-Id: I5d7740b0ea135896bb5a18ffb4ef15116e57880c
    Signed-off-by: noname55804 <marlin2201@gmail.com>

--- ch.mk
+++ ch.mk
@@ -1,3 +1,33 @@
+################OPTIMIZATIONS###################
+#Use Optimizations?
+USE_OPTIMIZATIONS := true
+
+ifneq ($(strip $(USE_OPTIMIZATIONS)),false)
+#HACKIFY CONFIGURATION 
+
+##Define ROM toolchain and LIB Verison(should be the same version number as the toolchain)
+TARGET_GCC_AND := 4.8-sm
+TARGET_LIB_VERSION := 4.8
+
+##Define Kernel toolchain
+TARGET_GCC_ARM := 4.9-sm
+
+##Enable Pthread (only on newer devices)
+ENABLE_PTHREAD := true
+
+##Enable O3 Optimizations
+O3_OPTIMIZATIONS:= true
+
+##Enable ArchiDroid Optimizations
+ARCHIDROID_OPTIMIZATIONS := true
+endif
+#################################################
+
+##If you´re running an old device, set this to true
+USE_LEGACY_GCC := false
+
+#$(call inherit-product, device/htc/m8/BoardConfig.mk)
+
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
 # Enhanced NFC
