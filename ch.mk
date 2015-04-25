72dcd679064cb7c8afc9d7247573cc5a9ec64791
noname55804 <marlin2201@gmail.com>
2015-04-25 21:00:49 +0200

    HUGE SaberMod update (3/4)
    This patch includes:
    
    from SM_vendor:
    MOAR SaberMod flags
    extra sabermod C flags
    Add more optimizations to extra sabermod gcc cflags and kernel flags
    Add more clang C flags
    enable graphite and -O3 form libwebviewchromium
    Add some basic host optimizations
    update sm.mk: cleanup graphite flags
    sm.mk: Don't add when exporting
    sm.mk: Fix device kernel graphite flags not being added
    Fix build, create sm_clear_vars.mk and add -pipe
    hammerhead: fix compiling on macs (darwin), and update sm_clear_vars.mk
    -O3 optimzations: More improvements
    SaberMod ARM Mode
    Fix bluetooth in SaberMod ARM Mode
    export GRAPHITE_UNROLL_AND_JAM to the kernel
    sm.mk: more dependency fixes:
    small cleanup
    
    from SM_build:
    MOAR SaberMod flags
    ARM: Enable -fno-builtin-sin and -fno-strict-volatile-bitfields by default
    extra sabermod flags binary mode: separate gcc from clang completely
    Add some basic host optimizations
    Cleanup graphite: LOCAL_CFLAGS will be used for CPP flags
    -O3 optimzations: More improvements
    Include sm_clear_vars.mk
    sm.mk: Bug fix for aosp and strict-aliasing
    SaberMod ARM Mode (2/2)
    small cleanup
    Rewrite SaberMod ARM Mode to fix build for arm64
    
    from SM_kernel:
    MOAR SaberMod flags
    pthread: make the warning more obvious
    Use common ld scripts for ld modules
    kernel: add support for gcc 5/6
    net: netfilter: disable loop-unroll-and-jam where needed
    Organize SaberMod edits and add LICENSE
    Update for latest rom changes.
    One more compatibility patch for graphite
    
    This reverts commit 3f32a4aef8a90f4b7977bc8a3da58cb9f39c5bd3.
    
    Change-Id: I6810b21816da69f2ed2e5d501cde84c5277d4dca

--- ch.mk
+++ ch.mk
@@ -3,6 +3,16 @@
 USE_OPTIMIZATIONS := true
 
 ifneq ($(strip $(USE_OPTIMIZATIONS)),false)
+include vendor/ch/config/sm_clear_vars.mk
+# Find host os
+UNAME := $(shell uname -s)
+
+ifeq ($(strip $(UNAME)),Linux)
+  HOST_OS := linux
+endif
+
+# Only use these compilers on linux host.
+ifeq ($(strip $(HOST_OS)),linux)
 #HACKIFY CONFIGURATION 
 
 ##Define ROM toolchain
@@ -17,12 +27,28 @@ TARGET_GCC_ARM := 4.9-sm
 ##Enable Pthread (only on newer devices)
 ENABLE_PTHREAD := true
 
-##Enable O3 Optimizations
-O3_OPTIMIZATIONS:= true
-
-#Use Kernel Optimizations?
+##Use Kernel Optimizations?
 USE_KERNEL_OPTIMIZATIONS := true
 
+##How many threads does the device have?
+PRODUCT_THREADS := 4
+
+# Extra SaberMod GCC C flags for arch target and Kernel
+export EXTRA_SABERMOD_GCC_CFLAGS := \
+         -ftree-vectorize \
+         -mvectorize-with-neon-quade
+
+# General flags for gcc 4.9 to allow compilation to complete.
+MAYBE_UNINITIALIZED := \
+  hwcomposer.msm8974
+
+LOCAL_DISABLE_STRICT_ALIASING := \
+  libmmcamera_interface\
+  camera.hammerhead
+
+else
+$(error *  Please compile on a Linux host OS to use this optimizations)
+endif
 endif
 #################################################
 
